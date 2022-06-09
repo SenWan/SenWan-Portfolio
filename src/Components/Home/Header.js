c5d526e49e64b32b3f91c3aa1cd14ef6d783e92b
U Sen Wan <senwan96@gmail.com>
2022-06-09 11:51:05 +0600

    header is added

---
+++ src/Components/Home/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css'
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div className='container p-3 mt-2 mb-3 d-flex justify-content-between align-items-center'>
+            <div>
+                <h2 className='text-black fw-bolder'>My Portfolio</h2>
+            </div>
+            <nav className='navbar d-flex'>
+                <CustomLink className="fw-bold" to="/">Home</CustomLink>
+                <CustomLink className="fw-bold" to="reviews">Contact</CustomLink>
+                <CustomLink className="fw-bold" to="about">About Me</CustomLink>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
