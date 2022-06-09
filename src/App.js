d18ae06eef3d0385c5e414bbc89f03c8121e7577
U Sen Wan <senwan96@gmail.com>
2022-06-09 17:46:48 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div>
+    <div className='full-bg'>
       <Home></Home>
     </div>
   );
--- src/Components/CustomLink/CustomLink.js
+++ src/Components/CustomLink/CustomLink.js
@@ -8,7 +8,7 @@ function CustomLink({ children, to, ...props }) {
     return (
       <div>
         <Link
-          style={{ color: match ?  "orange" : "green"}}
+          style={{ color: match ?  "orange" : "white"}}
           to={to}
           {...props}
         >
