835b19704cc671051efd79d7db354056f7904d7b
U Sen Wan <senwan96@gmail.com>
2022-06-09 18:13:27 +0600

    skills section added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Header from './Header';
+import Skills from './Skills';
 
 const Home = () => {
     return (
         <div className='container home'>
             <Header></Header>
             <Banner></Banner>
+            <Skills></Skills>
         </div>
     );
 };
---
+++ src/Components/Home/Skills.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Skills.css'
+
+const Skills = () => {
+    return (
+        <div className='text-white mt-5'>
+            <h2 className='text-center'>My Skills</h2>
+            <p>HTML</p>
+            <div className="skills-container">
+            <div class="skills html">90%</div>
+            </div>
+
+            <p>CSS</p>
+            <div className="skills-container">
+            <div className="skills css">80%</div>
+            </div>
+
+            <p>JavaScript</p>
+            <div className="skills-container">
+            <div className="skills js">60%</div>
+            </div>
+
+            <p>React</p>
+            <div className="skills-container">
+            <div className="skills react">70%</div>
+            </div>
+            
+            <p>Node.js</p>
+            <div className="skills-container">
+            <div className="skills node">40%</div>
+            </div>
+            
+            <p>MongoDB</p>
+            <div className="skills-container">
+            <div className="skills mongo">40%</div>
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
